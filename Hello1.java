8708a16676ded7e2bc83982c2ed076e12c6f92a0
ashwini <patilbashwini20@gmail.com>
2018-08-03 15:21:18 +0530

    Mymethod imp.....

--- Hello1.java
+++ Hello1.java
@@ -1,10 +1,13 @@
-class Hello{ 
+class Hello1{ 
 	void myMethod(){
 	}
 	public static void main(String[] args){
 		System.out.println("helloooo world______________Welcome to git");
 	
 	}
+
+
 	void heyMethod(){
+		sys("hbhjgj")
 	}
 }
